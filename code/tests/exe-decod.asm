c0192bac43b702dd9edbac586aa05a1671dbaddf
MathBatistela <42747473+MathBatistela@users.noreply.github.com>
2019-05-07 21:05:21 -0300

    base
    Initial files

---
+++ code/tests/exe-decod.asm
@@ -0,0 +1,28 @@
+.data
+
+
+.text
+	j MAIN
+
+MAIN: 	add $s1, $s2, $s3
+		add $zero, $zero, $zero
+		addi $t1, $t2, 5
+		sub $t1, $t2, $t3
+		lw $t0, 32($s7)
+		sw $s3, 128($0)
+		j TARGET
+		beq $s7, $zero, FIM
+		add $t1, $t2, $t3
+TARGET: 	nop
+		sll $t7, $s6, 5
+		jal FUNC
+		
+
+
+FUNC: nop
+	  jr $ra
+
+FIM:
+
+
+
